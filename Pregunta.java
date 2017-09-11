cfbe6730289ccd764d24a88b9e1d1d756f8f4ab0
braulioagr3 <31120570+braulioagr3@users.noreply.github.com>
2017-09-11 10:59:30 -0500

    Intercambio inicial de proyecto

---
+++ Pregunta.java
@@ -0,0 +1,27 @@
+/**
+ * Respresenta una pregunta.
+ * 
+ * @author (Braulio A. García Rivera) 
+ * @version (11/09/2017)
+ */
+public class Pregunta{
+    private String texto = "";
+    private String respuesta ="";
+    Pregunta()
+    {
+    
+    }
+    public void asignarTexto()
+    {
+    }
+    public void asignarRespuesta()
+    {
+    }
+    public boolean checarRespuesta()
+    {
+        return true;
+    }
+    public void imprimete()
+    {
+    }
+}
